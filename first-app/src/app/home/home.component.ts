60690ece135d3226814565aeccd156dea9f3cc62
iso9000t <sasha.gr81@gmail.com>
2023-10-12 23:08:58 -0300

    feat: add code for step 9

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -1,38 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 @Component({
   selector: "app-home",
   standalone: true,
-  imports: [
-    CommonModule,
-    HousingLocationComponent
-  ],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
+        <input type="text" placeholder="Filter by city" />
         <button class="primary" type="button">Search</button>
       </form>
     </section>
     <section class="results">
-      <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
+      <app-housing-location
+        *ngFor="let housingLocation of housingLocationList"
+        [housingLocation]="housingLocation"
+      ></app-housing-location>
     </section>
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
-  readonly baseUrl = "https://angular.io/assets/images/tutorials/faa";
+export class HomeComponent implements OnInit {
 
-  housingLocation: HousingLocation = {
-    id: 9999,
-    name: 'Test Home',
-    city: 'Test city',
-    state: 'ST',
-    photo: `${this.baseUrl}/example-house.jpg`,
-    availableUnits: 99,
-    wifi: true,
-    laundry: false,
-  };
+  housingLocationList: HousingLocation[] = [];
+
+  constructor(private housingService: HousingService) {}
+
+  ngOnInit(): void {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
